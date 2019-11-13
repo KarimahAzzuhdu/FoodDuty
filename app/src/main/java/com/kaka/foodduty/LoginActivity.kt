cfd9a953c8ed4d1709c4168440ef919970330dd1
KarimahAzzuhdu12 <kakazuto2912@gmail.com>
2019-11-13 14:21:34 +0700

    Initial commit

---
+++ app/src/main/java/com/kaka/foodduty/LoginActivity.kt
@@ -0,0 +1,18 @@
+package com.kaka.foodduty
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        btn_login.setOnClickListener {
+            startActivity(Intent(this, HomeActivity::class.java))
+        }
+    }
+
+}
